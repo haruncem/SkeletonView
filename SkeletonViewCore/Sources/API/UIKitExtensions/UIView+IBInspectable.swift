8ab3c97e8a7fa58a52732777020b5fe048d588bb
haruncem <haruncemdere@gmail.com>
2024-01-02 16:24:40 +0300

    Update UIView+IBInspectable.swift
    remove ibinspectables

--- SkeletonViewCore/Sources/API/UIKitExtensions/UIView+IBInspectable.swift
+++ SkeletonViewCore/Sources/API/UIKitExtensions/UIView+IBInspectable.swift
@@ -14,26 +14,22 @@
 import UIKit
 
 public extension UIView {
-    
-    @IBInspectable
+
     var isSkeletonable: Bool {
         get { _skeletonable }
         set { _skeletonable = newValue }
     }
-    
-    @IBInspectable
+
     var isHiddenWhenSkeletonIsActive: Bool {
         get { _hiddenWhenSkeletonIsActive }
         set { _hiddenWhenSkeletonIsActive = newValue }
     }
-    
-    @IBInspectable
+
     var isUserInteractionDisabledWhenSkeletonIsActive: Bool {
         get { _disabledWhenSkeletonIsActive }
         set { _disabledWhenSkeletonIsActive = newValue }
     }
 
-    @IBInspectable
     var skeletonCornerRadius: Float {
         get { _skeletonableCornerRadius }
         set { _skeletonableCornerRadius = newValue }
